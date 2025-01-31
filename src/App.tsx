2d67451306dd77667e0f4883fef0d7397fa1e8b8
Marwan Zogheib <mzogheib@users.noreply.github.com>
2025-02-01 10:00:11 +1100

    feat: update heading

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>vite-pwa-test</h1>
+      <h1>Vite PWA test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
